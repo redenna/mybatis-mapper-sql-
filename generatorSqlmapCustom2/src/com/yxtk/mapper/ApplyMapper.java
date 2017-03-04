0e829498182308c5fd910d7f54b24ae05e3e2a17
redenna <2661658075@qq.com>
2017-03-04 09:02:18 +0800

    Revert "add resource"
    This reverts commit 0ebf5975afd70d70f264bd1751577756d3736b25.

--- generatorSqlmapCustom2/src/com/yxtk/mapper/ApplyMapper.java
+++
@@ -1,8 +0,0 @@
-package com.yxtk.mapper;
-
-import com.yxtk.po.Apply;
-import com.yxtk.po.ApplyExample;
-import com.yxtk.vo.ApplyVo;
-
-public interface ApplyMapper extends BaseGenMapper<ApplyVo, Apply, ApplyExample, Integer> {
-}
--- generatorSqlmapCustom2/src/com/yxtk/mapper/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.yxtk.mapper;
-
-import com.yxtk.po.User;
-import com.yxtk.po.UserExample;
-
-public interface UserMapper extends BaseGenMapper<User, User, UserExample, Integer> {
-}
